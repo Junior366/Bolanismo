f76d20d5af8de427427a4d9f28c6a45349b2633a
junior366 <claudianosilvajunior373@gmail.com>
2023-06-01 12:26:12 -0300

    Initial commit

---
+++ src/Piloto.java
@@ -0,0 +1,12 @@
+public class Piloto {
+
+    public String nome;
+    public String sobrenome;
+    public int CPF;
+    public int RG;
+    public int telefone;
+    public String endereco;
+
+
+
+}
