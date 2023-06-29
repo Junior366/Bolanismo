55d43ca3f0a04fdc0f364e6cf1792f1a36415b54
junior366 <claudianosilvajunior373@gmail.com>
2023-06-29 11:08:34 -0300

    Balonismo

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,18 @@
 public class Pessoa {
+    public String nome;
+    public String sobrenome;
+    public String CPF;
+    public String RG;
+    public double telefone;
+    public String endereco;
+
+    public Pessoa(String nome,String sobrenome, String CPF, String RG, double telefone, String endereco){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.CPF = CPF;
+        this.RG = RG;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
 }
--- src/Piloto.java
+++ src/Piloto.java
@@ -1,11 +1,13 @@
 public class Piloto {
 
     public String nome;
-    public String sobrenome;
-    public int CPF;
-    public int RG;
-    public int telefone;
-    public String endereco;
+    public String CPF;
+    public String NumeroderegistroANAC;
+
+    public Piloto(String nome,String CPF, String numeroderegistroANAC) {
+
+    }
+
 
 
 
